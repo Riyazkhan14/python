3411e25b281bd31bf9e4152d3073f1be0dc04e51
Riyaz.b <riyaz.b@bet21onlineltd.onmicrosoft.com>
2022-11-11 18:23:10 +0530

    updated

---
+++ string_concatination.py
@@ -0,0 +1,10 @@
+first_name = "Riyaz"
+last_name = "Khan"
+full_name = first_name + last_name
+print (full_name)
+print (first_name + " " + last_name)
+# print (first_name + 3) error
+print (first_name + "3")
+print (first_name + " " + str(3))
+print (first_name * 3)
+print ("\U0001FAE0") #for Emoji
---
+++ summary_chap1.py
@@ -0,0 +1,21 @@
+#print
+print ("hello")
+#escape
+print ("hello \"Hello\' ")
+#escape sequence
+print ("line A\nline B")
+#escape as normal text
+print ("line \\nlineb)|")
+print (r"line A \n line B")
+#as calc
+print (2+3)
+print (3*4+(2-5))
+#variable
+Name="Riyaz"
+Age="16"
+print (Name + Age)
+#name rule of variable
+#variable cannot start with $#@%^&*()_!~- and Numbers
+#convention for variable naming
+user_one = "u1"
+User_One = "u2"

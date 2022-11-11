8b7e2b8ffc27006b8c57cd04766236a1dcb87f66
Riyaz.b <riyaz.b@bet21onlineltd.onmicrosoft.com>
2022-11-11 16:14:25 +0530

    Variables

--- variables.py
+++ variables.py
@@ -4,3 +4,11 @@ number1 = 4
 print (number1)
 name= 'Riyaz\'s'
 print (name)
+
+#variable rules
+# not start with number i.e. 1car
+# special char will throw error
+
+user_name_person = "Riyaz" #snake case
+UserNamePerson = "Khan" #camel Case
+print(user_name_person + " " + UserNamePerson)

f9f5fe657ec44ee530821d573c3264602fd42c70
Riyazkhan14 <21050074+Riyazkhan14@users.noreply.github.com>
2023-04-08 19:53:40 +0530

    New Python code

---
+++ comment.py
@@ -0,0 +1,2 @@
+#this is comment
+#this is information for the data.
---
+++ escape_sequence.py
@@ -0,0 +1,7 @@
+print("hello \"world\" world")
+print('I\'m Riyaz')
+print("line A\nLine B\nLine C")
+print("Sr.No.\t Name\t \t Class")
+print("this is backslash \\")
+print("This is double backslash \\\\")
+print("this last char eater\bh")

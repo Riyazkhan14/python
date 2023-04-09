99e3eec0f44d3184822df1678d707e318c0c42cc
Riyazkhan14 <21050074+Riyazkhan14@users.noreply.github.com>
2023-04-09 18:24:26 +0530

    new Python lesson

---
+++ exercise2.py
@@ -0,0 +1,6 @@
+n1 = int(input("Enter num 1: "))
+n2 = int(input("Enter num 2: "))
+n3 = int(input("Enter num 3: "))
+total = ((n1+n2+n3)/3)
+print(f"The average of three numbers are {total}")
+print(f"The avg of three numbers are : 
